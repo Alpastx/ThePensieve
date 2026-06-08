a183b09cc7e637117f13fb9e72d1cf8e28208d5e
Alpesh Bhagwatkar <alpeshbhagwatkar45@gmail.com>
2026-06-09 01:17:36 +0530

    Docs: Added web-recon-basics

--- app/Red-Teaming/01-Recon/_meta.js
+++ app/Red-Teaming/01-Recon/_meta.js
@@ -1,6 +1,7 @@
 const reconMeta = {
     'Nmap-Network-Enum': 'Nmap - Network Enumeration',
     'Service-Enum': 'Service enumeration',
+    'Web-Recon-Basic': 'Web Recon Basic',
 }
 
 export default reconMeta
