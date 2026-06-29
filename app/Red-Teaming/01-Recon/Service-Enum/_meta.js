90b539c4d04b0010f5a7f38fdfb8e3b1cc42a933
Alpesh Bhagwatkar <alpeshbhagwatkar45@gmail.com>
2026-06-29 21:50:41 +0530

    Doc: extended cheatsheet in IMAP

--- app/Red-Teaming/01-Recon/Service-Enum/_meta.js
+++ app/Red-Teaming/01-Recon/Service-Enum/_meta.js
@@ -1,7 +1,8 @@
 const serviceEnumMeta = {
-  'FTP-21': 'FTP/TFTP - 21,20,69',
-  'DNS-53': 'DNS - 53',
+  'FTP': 'FTP/TFTP - 21,20,69',
+  'DNS': 'DNS - 53',
   'IMAP': 'IMAP - 143, 993, 110, 995',
+  'IPMI': 'IPMI - 623',
 }
 
 export default serviceEnumMeta
