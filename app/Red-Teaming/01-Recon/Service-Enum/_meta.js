133d46220aa87c86af7f54e2d4091e417d48a3b8
Alpesh Bhagwatkar <alpeshbhagwatkar45@gmail.com>
2026-07-04 21:10:46 +0530

    Doc: added ipmi and mysql and added relevant hashtags + frontmaatter

--- app/Red-Teaming/01-Recon/Service-Enum/_meta.js
+++ app/Red-Teaming/01-Recon/Service-Enum/_meta.js
@@ -1,8 +1,10 @@
 const serviceEnumMeta = {
-  'FTP': 'FTP/TFTP - 21,20,69',
-  'DNS': 'DNS - 53',
-  'IMAP': 'IMAP - 143, 993, 110, 995',
+  'FTP': 'FTP/TFTP - 21,20,69(UDP)',
+  'DNS': 'DNS - 53(UDP,TCP)',
+  'IMAP': 'IMAP/POP3 - 143/993, 110/995',
   'IPMI': 'IPMI - 623',
+  'MSSQL': 'MSSQL- 1433,2433,1434(UDP)',
+  'mysql': 'MySQL - 3306',
 }
 
 export default serviceEnumMeta
